1bf048fe89506bb8334e9c5d451db94dac605346
tiomarello <58510532+tiomarello@users.noreply.github.com>
2020-04-19 14:11:56 -0400

    Added Enemy Scriptable Object

---
+++ Assets/Scripts/Entity/Enemies/EnemyObject.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="Enemy", menuName="Enemy")]
+public class EnemyObject : ScriptableObject{
+    public GameObject Enemy;
+    public Difficulty LevelToSpawnAt;
+    public int numberMultiplier;
+}
